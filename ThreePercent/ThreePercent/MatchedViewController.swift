fcec2c2455e04998aa0b049f70e118a338413241
eunji <twih1203@gmail.com>
2019-05-19 04:23:43 +0900

    MatchingViewController

---
+++ ThreePercent/ThreePercent/MatchedViewController.swift
@@ -0,0 +1,79 @@
+//
+//  MatchingViewController.swift
+//  ThreePercent
+//
+//  Created by DEV_MOBILE_IOS_JUNIOR on 18/05/2019.
+//  Copyright © 2019 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
+//
+
+import UIKit
+
+class MatchedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    
+    // MARK : - IBOutlet
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+
+    // MARK : - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        swipeHandler()
+        setPageControler()
+        
+        textLabel.text = "인연이라면\n다시\n만나겠죠"
+    }
+    
+    // MARK : - Layout
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get { return .lightContent }
+    }
+    
+    
+    // MARK : - IBAction
+    func swipeHandler() {
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        downSwipe.direction = .down
+        view.addGestureRecognizer(downSwipe)
+    }
+    
+    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
+        if (sender.direction == .down) {
+            tabBarController?.selectedIndex = 0
+        }
+    }
+    
+    // MARK : - UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MatchImageCollectionViewCell", for: indexPath)
+        return cell
+    }
+    
+    // MARK: - FlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+    
+    // MARK : - UICollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        self.pageControl.currentPage = indexPath.item
+    }
+
+    // MARK: - Private MEthod
+    func setPageControler() {
+        self.pageControl.currentPageIndicatorTintColor = UIColor.init(red: 245/255, green: 52/255, blue: 74/255, alpha: 1.0)
+        self.pageControl.numberOfPages = 3
+    }
+}
--- ThreePercent/ThreePercent/MatchingViewController.swift
+++ ThreePercent/ThreePercent/MatchingViewController.swift
@@ -1,79 +1,51 @@
 //
-//  MatchingViewController.swift
+//  MainViewController.swift
 //  ThreePercent
 //
-//  Created by DEV_MOBILE_IOS_JUNIOR on 18/05/2019.
+//  Created by DEV_MOBILE_IOS_JUNIOR on 19/05/2019.
 //  Copyright © 2019 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
 //
 
 import UIKit
 
-class MatchingViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+class MatchingViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    // MARK : - IBOutlet
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var indicator: UIView!
+    @IBOutlet weak var tabView: UIView!
     
-    
-
     // MARK : - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        swipeHandler()
-        setPageControler()
-        
-        textLabel.text = "인연이라면\n다시\n만나겠죠"
     }
     
-    // MARK : - Layout
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        get { return .lightContent }
-    }
-    
-    
-    // MARK : - IBAction
-    func swipeHandler() {
-        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
-        downSwipe.direction = .down
-        view.addGestureRecognizer(downSwipe)
-    }
-    
-    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
-        if (sender.direction == .down) {
-            tabBarController?.selectedIndex = 0
-        }
-    }
-    
-    // MARK : - UICollectionViewDataSource
+    //  MARK : - CollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MatchImageCollectionViewCell", for: indexPath)
-        return cell
-    }
-    
-    // MARK: - FlowLayout
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return collectionView.frame.size
-    }
-    
-    // MARK : - UICollectionViewDelegate
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        self.pageControl.currentPage = indexPath.item
+        
+        switch indexPath.item {
+        case 0:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "realTimeCell", for: indexPath)
+            return cell
+        default:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sendLoverCell", for: indexPath)
+            return cell
+            
+        }
     }
 
-    // MARK: - Private MEthod
-    func setPageControler() {
-        self.pageControl.currentPageIndicatorTintColor = UIColor.init(red: 245/255, green: 52/255, blue: 74/255, alpha: 1.0)
-        self.pageControl.numberOfPages = 3
+    // MARK : - CollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: collectionView.frame.width / 2, height: tabView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
     }
 }
