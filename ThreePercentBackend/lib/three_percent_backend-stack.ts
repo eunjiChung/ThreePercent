22b8d2b6534811f124a7646759e3d3e0b6e57561
Eunchong Yu <kroisse@gmail.com>
2019-05-19 02:57:56 +0900

    기본적인 사용자 추가 및 사진 업로드 API 구현

--- ThreePercentBackend/lib/three_percent_backend-stack.ts
+++ ThreePercentBackend/lib/three_percent_backend-stack.ts
@@ -9,17 +9,26 @@ export class ThreePercentBackendStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        const bucket = new Bucket(this, 'Bucket', {
+            bucketName: 'musseulmoton-3percent',
+        });
+
         const handler = new Function(this, 'Handler', {
             runtime: Runtime.NodeJS810,
             handler: 'index.handler',
             code: Code.asset(path.resolve(path.dirname(__dirname), 'lambda')),
-        });
-        new LambdaRestApi(this, 'Api', {
-            handler,
+            environment: {
+                BUCKET_NAME: bucket.bucketName,
+            },
         });
 
-        new Bucket(this, 'Bucket', {
-            bucketName: 'musseulmoton-3percent',
+        new LambdaRestApi(this, 'Api', {
+            handler,
+            options: {
+                deployOptions: {
+                    stageName: 'dev',
+                },
+            },
         });
     }
 }
